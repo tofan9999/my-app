257057e055b49238774914666cc04276092e98a3
tofan9999 <mohantatofan31@gmail.com>
2025-07-26 15:03:35 +0530

    all add

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,66 @@
 import Image from "next/image";
+import { IoCodeSlashSharp } from "react-icons/io5";
+import { HiAcademicCap } from "react-icons/hi2";
+import { PiAppleLogoFill } from "react-icons/pi";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <>
+      <div className="py-10 w-full h-full">
+        <div className="flex flex-col gap-2 justify-center items-center">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-sm text-center tracking-[0.1rem]">
+              Introduction
+            </h1>
+            <h1 className="text-3xl text-black/80">About me</h1>
+          </div>
+          <div className="w-full h-full flex justify-center items-center">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/Capture1.PNG"
+              width={1000}
+              height={1000}
+              alt=""
+              className="w-80 h-100"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        <div className="md:px-45 md:py-10 max-md:min-w-full max-md:px-4">
+          <p className="text-black/70 leading-8 text-justify ">
+            {" "}
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only
+            five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.{" "}
+          </p>
+        </div>
+        <div className="flex flex-row gap-15 px-35 justify-center items-center max-md:flex max-md:flex-col max-md:gap-4 max-md:py-10">
+          <div className="w-70 h-55 flex flex-col gap-5 rounded-2xl outline-1 outline-gray-300 p-8">
+            <IoCodeSlashSharp className="size-10"/>
+            <h1>Language</h1>
+            <p>Html,css,JavaScript,React Js,next Js
+            </p>
+          </div>
+           <div className="w-70 h-55 flex flex-col gap-5 rounded-2xl outline-1 outline-gray-300 p-8">
+            <HiAcademicCap  className="size-10"/>
+            <h1>Language</h1>
+            <p>Html,css,JavaScript,React Js,next Js
+            </p>
+          </div>
+           <div className="w-70 h-55 flex flex-col gap-5 rounded-2xl outline-1 outline-gray-300 p-8">
+            <PiAppleLogoFill className="size-10"/>
+            <h1>Language</h1>
+            <p>Html,css,JavaScript,React Js,next Js
+            </p>
+          </div>
+           
+           
+        </div>
+      </div>
+    </>
   );
 }
